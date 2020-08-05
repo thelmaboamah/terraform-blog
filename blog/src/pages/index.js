b0fd4a263e4432790d5a427a5d0e0c7f00c8fd59
Thelma Boamah <thelma.a.boamah@gmail.com>
2020-08-05 15:36:57 -0400

    Add gatsby hello world.

---
+++ blog/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+      <header><h1>Thelma's Cool Blog</h1></header>
+      <main>
+
+      </main>
+    </div>
+  )
+}
